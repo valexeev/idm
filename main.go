54eead30a725df58ce3a31a95cb2bf9e1879021b
valexeev <valekseev@inbox.ru>
2025-05-16 01:03:49 +0300

    feat: add uuid example

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func main() {
 	fmt.Println("Hello, Go.")
+
+	id := uuid.New()
+	fmt.Println("Generated UUID:", id.String())
 }
